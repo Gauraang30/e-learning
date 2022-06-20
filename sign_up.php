e75d5a1f20ea61c36e58f74d24aa28a4e2e7a2d1
Steins;ATLA <87314863+Gauraang30@users.noreply.github.com>
2022-06-20 00:03:46 -0700

    Update sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -3,19 +3,30 @@
 // $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 include('connection.php');
 // get the post records
-$txtName = $_POST['username'];
-$txtEmail = $_POST['email'];
-$txtPhone = $_POST['password'];
+try{
+$txtName = $_POST['inputName3'];
+$txtEmail = $_POST['inputEmail3'];
+$txtPhone = $_POST['inputPassword3'];
 
 // database insert SQL code
-$sql = "INSERT INTO `register` (`Id`, `name`, `email`, `password`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone')";
+$sql = "INSERT INTO `student` (`Id`, `name`, `email`, `password`) VALUES ('0', '$txtName', '$txtEmail', '$txtPhone')";
 
 // insert in database 
 $rs = mysqli_query($con, $sql);
-
 if($rs)
 {
-	echo "Contact Records Inserted";
+	echo '<script type="text/JavaScript"> 
+	alert("Sign-up Successful");
+	window.location.href="index.php";
+	</script>';
 }
-
+}
+catch(mysqli_sql_exception $e){
+	if($e->getCode()==1062){
+	echo'<script type="text/JavaScript"> 
+	alert("E-mail already in use");
+	window.location.href="index.php";
+	</script>';
+}
+}
 ?>
